80d6ed226825c904511e6ed8480a60fe5249fc4e
leonelcolina <tang.leonel@gmail.com>
2024-06-02 00:05:11 -0400

    Phase 4 completed

--- server/routes/students.js
+++ server/routes/students.js
@@ -4,119 +4,100 @@ const router = express.Router();
 
 // Import model(s)
 const { Student } = require('../db/models');
-const { Op } = require("sequelize");
+const { Op, Sequelize } = require("sequelize");
 
 // List
 router.get('/', async (req, res, next) => {
-    let errorResult = { errors: [], count: 0, pageCount: 0 };
+  let errorResult = { errors: [], count: 0, pageCount: 0 };
 
-    let page = req.query.page;
+  let page = req.query.page;
 
-    if(page === '0'){
-        page = null;
-    }else if(page === undefined){
-        page = 1;
-    }else{
-        page = parseInt(page)
-    }
+  if (page === "0") {
+    page = null;
+  } else if (page === undefined) {
+    page = 1;
+  } else {
+    page = parseInt(page);
+  }
 
-    let size = req.query.size;
+  let size = req.query.size;
 
-    if(size === '0'){
-        size = null;
-    }else if(size === undefined){
-        size = 10;
-    }else{
-        size = parseInt(size)
-    }
+  if (size === "0") {
+    size = null;
+  } else if (size === undefined) {
+    size = 10;
+  } else {
+    size = parseInt(size);
+  }
 
+  //If the parameters are invalid, populate the errorResult object
+  if (String(size) === "NaN" || String(page) === "NaN") {
+    errorResult.errors.push({ message: "Requires valid page and size params" });
+  }
 
-    if(String(size) === 'NaN' || String(page) === 'NaN'){
-        errorResult.errors.push({message: 'Requires valid page and size params'});
-    }
+  const where = {};
 
-    const offset = size * (page - 1)
+//   firstName filter:
+  if(req.query.firstName){
+      where.firstName = {[Op.substring]: req.query.firstName}
+  }
 
-    // Phase 4: Student Search Filters
-    /*
-        firstName filter:
-            If the firstName query parameter exists, set the firstName query
-                filter to find a similar match to the firstName query parameter.
-            For example, if firstName query parameter is 'C', then the
-                query should match with students whose firstName is 'Cam' or
-                'Royce'.
+//   lastName filter:
+  if(req.query.lastName){
+    where.lastName = { [Op.substring]: req.query.lastName};
+  }
 
-        lastName filter: (similar to firstName)
-            If the lastName query parameter exists, set the lastName query
-                filter to find a similar match to the lastName query parameter.
-            For example, if lastName query parameter is 'Al', then the
-                query should match with students whose lastName has 'Alfonsi' or
-                'Palazzo'.
+//   leftHanded filter:
+  if(req.query.leftHanded === 'true'){
+    where.leftHanded = { [Op.eq]: true };
+  } else if(req.query.leftHanded === 'false'){
+    where.leftHanded = {[Op.eq]: false}
+  }else if(req.query.leftHanded !== undefined){
+    errorResult.errors.push({message: 'Lefty should be either true or false'})
+  }
 
-        lefty filter:
-            If the lefty query parameter is a string of 'true' or 'false', set
-                the leftHanded query filter to a boolean of true or false
-            If the lefty query parameter is neither of those, add an error
-                message of 'Lefty should be either true or false' to
-                errorResult.errors
-    */
-    const where = {};
+  let result = {};
+  // Calculate the offset
+  const offset = size * (page - 1);
 
-    // Your code here
+  // Query and calculate the total of students and add it to the error
+  let total = await Student.findAll({
+    attributes: {
+      include: [
+        [Sequelize.fn("COUNT", Sequelize.col("leftHanded")), "total"],
+        "id",
+        "firstName",
+        "lastName",
+        "leftHanded",
+      ],
+    },
+    where,
+    order: [["lastName"], ["firstName"]],
+  });
+  total = total[0].dataValues.total;
 
+  //Calculate the number of pages
+  const pageCount = Math.ceil(total / size);
 
-    // Phase 3C: Include total student count in the response even if params were
-        // invalid
-        /*
-            If there are elements in the errorResult.errors array, then
-            return a "Bad Request" response with the errorResult as the body
-            of the response.
+  if (errorResult.errors.length > 0) {
+    errorResult.count = total;
+    res.statusCode = 400;
+    res.send(errorResult);
+  }
 
-            Ex:
-                errorResult = {
-                    errors: [{ message: 'Grade should be a number' }],
-                    count: 267,
-                    pageCount: 0
-                }
-        */
-    // Your code here
-    if(errorResult.errors.length > 0){
-        res.statusCode = 400;
-        res.send(errorResult)
-    }
+  // Add all the values to the result object
+  result.rows = await Student.findAll({
+    attributes: ["id", "firstName", "lastName", "leftHanded"],
+    where,
+    order: [["lastName"], ["firstName"]],
+    limit: size,
+    offset,
+  });
+  result.page = page || 1;
+  result.count = total;
+  result.pageCount = pageCount;
 
-    let result = {};
-
-    // Phase 3A: Include total number of results returned from the query without
-        // limits and offsets as a property of count on the result
-        // Note: This should be a new query
-
-    result.rows = await Student.findAll({
-        attributes: ['id', 'firstName', 'lastName', 'leftHanded'],
-        where,
-        order: [['lastName'],['firstName']],
-        limit: size,
-        offset
-    });
-    result.page = page || 1;
-   
-    // Phase 3B:
-        // Include the total number of available pages for this query as a key
-            // of pageCount in the response data
-        // In the special case (page=0, size=0) that returns all students, set
-            // pageCount to 1
-        /*
-            Response should be formatted to look like this:
-            {
-                count: 17 // total number of query results without pagination
-                rows: [{ id... }] // query results,
-                page: 2, // current page of this query
-                pageCount: 10 // total number of available pages for this query
-            }
-        */
-    // Your code here
-
-    res.json(result);
+  res.json(result);
 });
 
 // Export class - DO NOT MODIFY
